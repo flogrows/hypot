71e8f15598f86af232cafb3768d4670af7d870f4
Flo <75952091+flogrows@users.noreply.github.com>
2024-07-25 13:37:21 +0000

    Added function and test file

---
+++ src/hypot/source.py
@@ -0,0 +1,11 @@
+def thing(a,b):
+    """Adds a and b
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a+b
